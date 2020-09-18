217f15e9a4a0591accbc1cad0d23f46bfe3838e0
matthieugomez <gomez.matthieu@gmail.com>
2020-09-17 17:54:23 -0700

    Do plot

--- src/Binscatters.jl
+++ src/Binscatters.jl
@@ -3,44 +3,43 @@ module Binscatters
 using DataFrames
 using Statistics
 using FixedEffectModels
+using RecipesBase
+using CategoricalArrays
 
 """
-Return a DataFrame that (i) residualizes the left hand side and the first variable of the right hand side w.r.t all other variables in the formula (ii) average variables w.r.t bins of the first variable of the right-hand-side
+Return a plot after (i) residualizeing the left hand side and the first variable of the right hand side w.r.t all other variables in the formula (ii) averaging variables w.r.t bins of the first variable of the right-hand-side
 
 ### Arguments
-* `df`: a DataFrame
-* `FormulaTerm`: A formula created using [`@formula`](@ref)
+* `df`: a DataFrame or a GroupedDataFrame
+* `FormulaTerm`: A formula created using [`@formula`](@ref). The syntax is `@formula(y1 + y2 ~ x + control1 + control2)` 
 * `weights`: A symbol for weights
 * `n`: Number of groups
+* kwargs...: Plot attributes from Plots
 
 ### Examples
 ```julia
-using Binscatters, RDatasets
+using Revise, Binscatters, RDatasets, Plots
+pgfplotsx()
 df = dataset("plm", "Cigar")
-binscatter(df, @formula(Sales ~ NDI))
-binscatter(df, @formula(Sales ~ NDI + fe(State)))
-binscatter(df, @formula(Sales ~ NDI + Price + fe(State)))
-binscatter(df, @formula(Sales + Price ~ NDI + fe(State)))
+binscatter(df, @formula(Sales ~ Price))
+binscatter(df, @formula(Sales ~ Price), n = 10)
+binscatter(df, @formula(Sales ~ Price), n = 10, color = :black)
+
+# Residualize with respect to variables
+binscatter(df, @formula(Sales ~ Price + NDI))
+binscatter(df, @formula(Sales ~ Price + NDI + fe(Year)))
+
+# binscatter multiple variables
+binscatter(df, @formula(Sales + Price ~ NDI))
+
+# binscatter by groups
+df.Year2 = df.Year .>= 70
+binscatter(groupby(df, :Year2), @formula(Sales ~ Price + fe(Year)))
+df.State2 = df.State .>= 25
+binscatter(groupby(df, [:Year2, :State2]), @formula(Sales ~ Price + fe(Year)))
 ```
 """
-
-function binscatter(df::AbstractDataFrame, @nospecialize(f::FormulaTerm); weights::Union{Symbol, Nothing} = nothing, n = 20)
-    df = partial_out(df, translate(f); weights = weights, align = false, add_mean = true)[1]
-    cols = names(df)
-    df.__cut = cut(df[!, end], n; allowempty = true)
-    df = groupby(df, :__cut)
-    combine(df, cols .=> mean .=> cols; keepkeys = false)
-end
-
-function binscatter(df::GroupedDataFrame, @nospecialize(f::FormulaTerm); weights::Union{Symbol, Nothing} = nothing, n = 20)
-    df = combine(d -> binscatter(d, f; weights = weights, n = n), df)
-    df = stack(df, names(df, 2:size(df, 2)); 
-        variable_name = :__variable, value_name = :__value, variable_eltype = String)
-    df.__variable = names(df, 1) .* "=" .* string.(df[!, 1]) .* df.__variable 
-    select!(df, :__variable, :__value)
-    transform!(groupby(df, :__variable), :__value => (x -> 1:length(x)) => :length)
-    unstack(df, :__variable, :__value)
-end
+binscatter
 
 function translate(@nospecialize(formula::FormulaTerm))
     lhs = formula.lhs
@@ -53,6 +52,46 @@ function translate(@nospecialize(formula::FormulaTerm))
     end
     i = findfirst(x -> x isa Term, rhs)
     FormulaTerm(tuple(lhs..., rhs[i]), Tuple(term for term in rhs if term != rhs[i]))
+end
+
+function bin(df::AbstractDataFrame, @nospecialize(f::FormulaTerm); weights::Union{Symbol, Nothing} = nothing, n = 20)
+    df = partial_out(df, translate(f); weights = weights, align = false, add_mean = true)[1]
+    cols = names(df)
+    df.__cut = cut(df[!, end], n; allowempty = true)
+    df = groupby(df, :__cut)
+    combine(df, cols .=> mean .=> cols; keepkeys = false)
+end
+
+function bin(df::GroupedDataFrame, @nospecialize(f::FormulaTerm); weights::Union{Symbol, Nothing} = nothing, n = 20, ungroup = true)
+    combine(d -> bin(d, f; weights = weights, n = n), df; ungroup = ungroup)
+end
+
+#user recipe
+@userplot Binscatter
+@recipe function f(bs::Binscatter; weights = nothing, n = 20)
+    df = bs.args[1]
+    f = bs.args[2]
+    seriestype --> :scatter
+    if df isa DataFrame
+        df = bin(df, f; weights = weights, n = n)
+        cols = names(df)
+        xguide := cols[end]
+        N = length(cols)
+        label := reshape(cols[1:(end-1)], 1, N-1)
+        df[!, end], collect(eachcol(df[!, 1:(N-1)]))
+    else
+        gcols = groupcols(df)
+        df = bin(df, f; weights = weights, n = n, ungroup = false)
+        for (k, out) in pairs(df)
+            @series begin
+                cols = setdiff(names(out), string.(gcols))
+                xguide := cols[end]
+                N = length(cols)
+                label := reshape(cols[1:(end-1)], 1, N-1)  .* " " .* string(NamedTuple(k))
+                out[!, end], collect(eachcol(out[!, (length(gcols)+1):(end-1)]))
+            end
+        end
+    end
 end
 
 
