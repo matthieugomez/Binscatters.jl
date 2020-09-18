a35ace7297189c565588781b34f3c4f79784e407
matthieugomez <gomez.matthieu@gmail.com>
2020-09-18 11:13:26 -0700

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,13 +23,13 @@ bin(df, @formula(y+x2~x1))
 bin(groupby(df, :id1), @formula(y~x1))
 
 binscatter(df, @formula(y ~ x1))
-binscatter(df, @formula(y ~ x1), linekin = :connect)
-binscatter(df, @formula(y ~ x1), linekin = :lfit)
+binscatter(df, @formula(y ~ x1), kind = :connect)
+binscatter(df, @formula(y ~ x1), kind = :lfit)
 
 
 binscatter(df, @formula(y + x2 ~ x1))
-binscatter(df, @formula(y + x2 ~ x1), linekin = :connect)
-binscatter(df, @formula(y + x2 ~ x1), linekin = :lfit)
+binscatter(df, @formula(y + x2 ~ x1), kind = :connect)
+binscatter(df, @formula(y + x2 ~ x1), kind = :lfit)
 
 
 
@@ -41,5 +41,5 @@ binscatter(df, @formula(y~x1+x2))
 
 df.dummy = df.id1 .>= 25
 binscatter(groupby(df, :dummy), @formula(y~x1))
-binscatter(groupby(df, :dummy), @formula(y~x1), linekin = :connect)
-binscatter(groupby(df, :dummy), @formula(y~x1), linekin = :lfit)
+binscatter(groupby(df, :dummy), @formula(y~x1), kind = :connect)
+binscatter(groupby(df, :dummy), @formula(y~x1), kind = :lfit)
