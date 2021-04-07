bf2d518ab4e47cff392a03a96cd922258bafd13b
matthieugomez <gomez.matthieu@gmail.com>
2021-04-07 12:44:34 -0700

    add sorting test + tag new version

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,50 +1,46 @@
 
 using CSV, DataFrames, Test, Plots, Binscatters
 df = DataFrame(CSV.File(joinpath(dirname(pathof(Binscatters)), "../dataset/Cigar.csv")))
-df.id1 = df.State
-df.y = df.Sales
-df.x1 = df.Price
-df.z1 = df.Pimin
-df.x2 = df.NDI
-df.w = df.Pop
-
-
-out = Binscatters.bin(df, @formula(y~x1))
-@test minimum(out.x1) >= minimum(df.x1)
-@test maximum(out.x1) <= maximum(df.x1)
-#Binscatters.bin(df, @formula(y~x1), weights = :w)
-#@test minimum(out.x1) >= minimum(df.x1)
-#@test maximum(out.x1) <= maximum(df.x1)
-Binscatters.bin(df, @formula(y~x1), 10)
-Binscatters.bin(df, @formula(y~x1+x2))
-Binscatters.bin(df, @formula(y ~ x1 + fe(id1)))
-Binscatters.bin(df, @formula(y+x2~x1))
-
-Binscatters.bin(groupby(df, :id1), @formula(y~x1))
-
-binscatter(df, @formula(y ~ x1))
-binscatter(df, @formula(y ~ x1), seriestype = :scatterpath)
-binscatter(df, @formula(y ~ x1), seriestype = :linearfit)
-
-
-binscatter(df, @formula(y + x2 ~ x1))
-binscatter(df, @formula(y + x2 ~ x1), seriestype = :scatterpath)
-binscatter(df, @formula(y + x2 ~ x1), seriestype = :linearfit)
 
 
 
-binscatter(df, @formula(y ~ x1 + fe(id1)))
-binscatter(df, @formula(y~x1), weights = :w)
-binscatter(df, @formula(y+x2~x1))
-binscatter(df, @formula(y~x1+x2))
+out = Binscatters.bin(df, @formula(Sales ~ Price))
+@test issorted(out.Price)
+@test minimum(out.Price) >= minimum(df.Price)
+@test maximum(out.Price) <= maximum(df.Price)
+#Binscatters.bin(df, @formula(Sales ~ Price), weights = :w)
+#@test minimum(out.Price) >= minimum(df.Price)
+#@test maximum(out.Price) <= maximum(df.Price)
+Binscatters.bin(df, @formula(Sales ~ Price), 10)
+Binscatters.bin(df, @formula(Sales ~ Price + NDI))
+Binscatters.bin(df, @formula(Sales ~ Price + fe(State)))
+Binscatters.bin(df, @formula(Sales + NDI ~ Price))
+
+Binscatters.bin(groupby(df, :State), @formula(Sales ~ Price))
+
+binscatter(df, @formula(Sales ~ Price))
+binscatter(df, @formula(Sales ~ Price), seriestype = :scatterpath)
+binscatter(df, @formula(Sales ~ Price), seriestype = :linearfit)
 
 
-df.dummy = df.id1 .>= 25
-binscatter(groupby(df, :dummy), @formula(y~x1))
-binscatter(groupby(df, :dummy), @formula(y~x1), seriestype = :scatterpath)
-binscatter(groupby(df, :dummy), @formula(y~x1), seriestype = :linearfit)
+binscatter(df, @formula(Sales + NDI ~ Price))
+binscatter(df, @formula(Sales + NDI ~ Price), seriestype = :scatterpath)
+binscatter(df, @formula(Sales + NDI ~ Price), seriestype = :linearfit)
 
 
-binscatter(groupby(df, :dummy), @formula(y+x2~x1))
-binscatter(groupby(df, :dummy), @formula(y+x2~x1), seriestype = :scatterpath)
-binscatter(groupby(df, :dummy), @formula(y+x2~x1), seriestype = :linearfit)
+
+binscatter(df, @formula(Sales ~ Price + fe(State)))
+binscatter(df, @formula(Sales ~ Price), weights = :Pop)
+binscatter(df, @formula(Sales + NDI~ Price))
+binscatter(df, @formula(Sales ~ Price + NDI))
+
+
+df.dummy = df.State .>= 25
+binscatter(groupby(df, :dummy), @formula(Sales ~ Price))
+binscatter(groupby(df, :dummy), @formula(Sales ~ Price), seriestype = :scatterpath)
+binscatter(groupby(df, :dummy), @formula(Sales ~ Price), seriestype = :linearfit)
+
+
+binscatter(groupby(df, :dummy), @formula(Sales + NDI ~ Price))
+binscatter(groupby(df, :dummy), @formula(Sales + NDI ~ Price), seriestype = :scatterpath)
+binscatter(groupby(df, :dummy), @formula(Sales + NDI ~ Price), seriestype = :linearfit)
