cee3b1a8db3b744b5d153eb55f1ac9856312745a
Matthieu Gomez <gomez.matthieu@gmail.com>
2022-09-24 09:57:39 -0400

    support  FunctionTerm
    solves https://github.com/matthieugomez/Binscatters.jl/issues/8

--- src/utils.jl
+++ src/utils.jl
@@ -64,7 +64,7 @@ function _shift(@nospecialize(formula::FormulaTerm))
     if !(rhs isa Tuple)
         rhs = (rhs,)
     end
-    i = findfirst(x -> x isa Term, rhs)
+    i = findfirst(x -> !(x isa ConstantTerm), rhs)
     FormulaTerm(tuple(lhs..., rhs[i]), Tuple(term for term in rhs if term != rhs[i]))
 end
 
